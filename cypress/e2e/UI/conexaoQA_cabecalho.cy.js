75cecfcab998affcb5c57b73bfabcb57fa457f55
Anderson Soares <anfrancisco@gmail.com>
2023-06-09 23:23:32 -0300

    adcionado novos testes de cabeçalho

--- cypress/e2e/UI/conexaoQA_cabecalho.cy.js
+++ cypress/e2e/UI/conexaoQA_cabecalho.cy.js
@@ -44,6 +44,40 @@ describe('cabeçalho de página home', () => {
             
         })
 
+        it('valida os menus utilizando Arrays', () => {
+
+            const menus = [
+                {seletor: 'navbar-conexaoQA', link: '/'},
+                {seletor: 'navbar-QAs', link: '/perfis'},
+                {seletor: 'navbar-about', link: '/sobre'},
+                {seletor: 'navbar-register', link: '/cadastrar'},
+                {seletor: 'navbar-login', link: '/login'}
+            ]
+
+            menus.forEach(({seletor, link})=> {
+                cy.get(`[data-test=${seletor}]`)
+                    .should('have.attr','href',link)
+            })
+        })
+
+        // independência dos testes
+        ;[
+            {seletor: 'navbar-conexaoQA', link: '/', menu: ' ConexãoQA'},
+            {seletor: 'navbar-QAs', link: '/perfis', menu: 'QAs'},
+            {seletor: 'navbar-about', link: '/sobre', menu: 'Sobre'},
+            {seletor: 'navbar-register', link: '/cadastrar', menu: 'Cadastrar'},
+            {seletor: 'navbar-login', link: '/login', menu: 'Login'}
+        ].forEach(({seletor, link, menu }) => {
+            it.only(`valida o menu ${menu}`, () => {
+                cy.get(`[data-test=${seletor}]`)
+                    .should('have.attr','href',link)
+                    .and('not.have.attr', 'target')
+
+            // .and('have.text', menu)
+            })
+
+        })
+
     })
 
     context('logado', () => {
