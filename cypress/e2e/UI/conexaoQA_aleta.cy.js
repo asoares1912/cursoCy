d49e87471ef7dd27dfb0028d1338b3bdbf30b371
Anderson Soares <anfrancisco@gmail.com>
2023-06-19 12:03:22 -0300

    Validando credencial inválida

---
+++ cypress/e2e/UI/conexaoQA_aleta.cy.js
@@ -0,0 +1,36 @@
+describe('Alerta de login', () => {
+    
+    it('valida o alerta de credecial invalida', () => {
+
+        cy.intercept('POST', '/api/auth')
+            .as ('login')
+
+        cy.visit('/login')
+
+        // preencher um e-mail aleatorio
+
+        cy.getElement('login-email')
+            .type('usuarioaleAtorio@teste.com')
+
+        // preencher uma senha aleatorio
+        cy.getElement('login-password')
+            .type('123456')
+
+        // clicar no botão login
+        cy.getElement('login-submit')
+            .click()
+
+        cy.wait('@login')
+
+        // validar o alerta de crendencial inválido
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+            
+        // cy.wait(10000)
+
+        // cy.getElement('alert')
+        cy.get('[data-test=alert]', { timeout: 10000})
+            .should('not.exist')
+    })
+    
+})
